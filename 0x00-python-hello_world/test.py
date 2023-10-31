a87527f0d5afc626eece66ac5b54f8f5ec696af6
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-10-31 13:41:12 +0100

    py

--- 0x00-python-hello_world/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-# min
-strt = str.index('object')
-# ila
-endd = str.index('programming') + len('programming')
-print(str[strt:endd])
