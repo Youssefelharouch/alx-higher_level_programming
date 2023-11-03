43f4a10440dbca250ab35493924b4c746f8c2256
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-11-03 11:58:27 +0100

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     import sys
 
-    argucnt = len(sys.argv) - 1
-    # our condi that will demonstaret our program
-    if argucnt == 0:
-        print("0 arguments.")
-    elif argucnt == 1:
-        print('1 argument:')
-    else:
-        print('{} arguments:'.format(argucnt))
-    for i in range(argucnt):
-        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(count))
+for i in range(count):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/py_print.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def py_print():
-    print("#pythoniscool")
-
-
-py_print()
