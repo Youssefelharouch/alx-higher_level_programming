2757596ce9277c932c26fa1e78e1b913f5e17d14
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-11-05 12:24:29 +0100

    ex

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return element
-    tmp_list = my_list[:]
-    tmp_list[idx] = element
-    return tmp_list
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
